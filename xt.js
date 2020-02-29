2dc0149a5a2a71b9e42e72b645211a805e038e60
Rusu Traian Cristian <RusuTraianCristian@users.noreply.github.com>
2020-02-29 21:38:10 +0200

    latest

--- xt.js
+++ xt.js
@@ -22,7 +22,7 @@ async function twitchFetch(url) {
         const response = await fetch(url, options);
         const data = await response.json();
         return data;
-    }dasddasdadasddasdsadasda15
+    }dasddasdadasddasdsadasda16
 
     catch (err) {
         console.error(err);
